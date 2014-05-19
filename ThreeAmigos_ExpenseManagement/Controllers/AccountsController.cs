e0d8e596aabf316a92fae67820741ef9c33e9aa0
JohnQuan <quan.le.john@gmail.com>
2014-05-19 13:47:24 +1000

    Add Accounts

---
+++ ThreeAmigos_ExpenseManagement/Controllers/AccountsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.EntityClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using ThreeAmigos_ExpenseManagement.BusinessLogic;
+using ThreeAmigos_ExpenseManagement.Filters;
+using ThreeAmigos_ExpenseManagement.Models;
+using ThreeAmigos_ExpenseManagement.ViewModels;
+
+namespace ThreeAmigos_ExpenseManagement.Controllers
+{
+    public class AccountsController : Controller
+    {
+        //
+        // GET: /Accounts/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
