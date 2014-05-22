a34ee191d4c35e50612fbf50342bda65eb97656c
bikrem <bikrem.singh@yahoo.in>
2014-05-22 05:58:43 +0530

    Added method to get report by supervisor

--- ThreeAmigos_ExpenseManagement/DataAccess/ExpenseReportDAL.cs
+++ ThreeAmigos_ExpenseManagement/DataAccess/ExpenseReportDAL.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using ThreeAmigos_ExpenseManagement.Models;
+using ThreeAmigos_ExpenseManagement.BusinessLogic;
 
 namespace ThreeAmigos_ExpenseManagement.DataAccess
 {
@@ -30,5 +32,24 @@ namespace ThreeAmigos_ExpenseManagement.DataAccess
         {
             throw new NotImplementedException();
         }
+
+
+        public List<ExpenseReport> GetReportsBySupervisor(string status)
+        {
+     
+            IEmployeeService employeeService = new EmployeeService();
+            DateTime startDateTime = DateTime.Today; //Today at 00:00:00 NEED TO CORRECT
+            DateTime endDateTime = DateTime.Today.AddDays(1).AddTicks(-1);
+            Employee employee = employeeService.GetEmployee((int)Membership.GetUser().ProviderUserKey);
+            using (EMEntitiesContext ctx = new EMEntitiesContext())
+            {
+                var result = (from i in ctx.ExpenseReports.Include("CreatedBy").Include("ExpenseItems").Include("Department")
+                              where i.Department.DepartmentId == employee.Department.DepartmentId && i.CreateDate >= startDateTime && i.CreateDate <= endDateTime && i.Status==status
+                              select i);
+                return (List<ExpenseReport>)result.ToList();              
+            }         
+        }
+
+
     }
 }
