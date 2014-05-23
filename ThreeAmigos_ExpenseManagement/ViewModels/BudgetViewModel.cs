8c29cd9d4c61ede0173df1de106de59917106986
bikrem <bikrem.singh@yahoo.in>
2014-05-24 04:47:10 +1000

    Added BudgetViewmodel

---
+++ ThreeAmigos_ExpenseManagement/ViewModels/BudgetViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThreeAmigos_ExpenseManagement.ViewModels
+{
+    public class BudgetViewModel
+    {
+        public decimal? totalAmountSpent { get; set; }
+        public decimal? totalAmountRemaining { get; set; }
+    }
+}
