6342ff535c01cffa5e0dc1f722892d89f05b91aa
bikrem <bikrem.singh@yahoo.in>
2014-05-24 04:43:40 +1000

    Added interface for budget tracker class

---
+++ ThreeAmigos_ExpenseManagement/BusinessLogic/IBudgetTracker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ThreeAmigos_ExpenseManagement.Models;
+
+namespace ThreeAmigos_ExpenseManagement.BusinessLogic
+{
+    public interface IBudgetTracker
+    {
+        decimal? DepartmentBudget(decimal? deptBudget, int? deptId);
+        decimal? CheckReportTotal(int? expenseId);
+        bool IsBudgetExceeded(decimal? amount);
+    }
+}
