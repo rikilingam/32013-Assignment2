0add1518336ba050108b4437d6d4032f8d86cfe0
bikrem <bikrem.singh@yahoo.in>
2014-05-24 04:45:05 +1000

    Updated supervisor controller

--- ThreeAmigos_ExpenseManagement/Controllers/SupervisorController.cs
+++ ThreeAmigos_ExpenseManagement/Controllers/SupervisorController.cs
@@ -16,35 +16,88 @@ namespace ThreeAmigos_ExpenseManagement.Controllers
     public class SupervisorController : Controller
     {
         IExpenseReportService reportService = new ExpenseReportService();
-        //
-        // GET: /Supervisor/
-
+        private IEmployeeService employeeService;
+        private Employee employee;
+        BudgetTracker bud;
+        int month = DateTime.Now.Month;
+        
+        public SupervisorController()
+        {
+            reportService = new ExpenseReportService();
+            employeeService = new EmployeeService();
+            bud = new BudgetTracker();            
+            employee = employeeService.GetEmployee((int)Membership.GetUser().ProviderUserKey);
+        }
+        
         public ActionResult Index()
         {
             return View();
         }
 
-
-        public ActionResult ApproveExpenses()
+        public ActionResult ApproveExpenses(int? itemid, string act)
         {
+            decimal? reportTotal = 0;
             ExpenseFormViewModel expenseForm = new ExpenseFormViewModel();
-            expenseForm.ExpenseReports = reportService.GetReportsBySupervisor("Submitted");
-            return View(expenseForm);
 
+            if (itemid == null)
+            {
+                expenseForm.ExpenseReports = reportService.GetReportsBySupervisor("Submitted", month);
+                return View(expenseForm);
+            }
+
+            else
+            {
+                if (act == "Approve")
+                {
+                    BudgetViewModel budget = new BudgetViewModel();
+                    budget.totalAmountSpent = bud.DepartmentBudget(employee.Department.MonthlyBudget, employee.DepartmentId);
+                    budget.totalAmountRemaining = bud.RemainingAmount;
+                    
+                    
+               
+                    reportTotal = bud.CheckReportTotal(itemid);
+                    if (bud.IsBudgetExceeded(reportTotal))
+                    {
+                        return View(expenseForm);
+                    }
+
+                    else
+                    {
+                        reportService.ActionOnReport(itemid, act);
+                        expenseForm.ExpenseReports = reportService.GetReportsBySupervisor("Submitted", month);
+                        return View(expenseForm);
+                    }
+                }
+                else
+                {
+                    reportService.ActionOnReport(itemid, act);
+                    expenseForm.ExpenseReports = reportService.GetReportsBySupervisor("Submitted", month);
+                    return View(expenseForm);
+                }
+            }
+        }   
+
+        public ActionResult ViewReports()
+        {
+            return View();
         }
 
         [HttpPost]
         public ActionResult ViewReports(string status)
         {
             ExpenseFormViewModel expenseForm = new ExpenseFormViewModel();
-            expenseForm.ExpenseReports = reportService.GetReportsBySupervisor(status);
+            expenseForm.ExpenseReports = reportService.GetReportsBySupervisor(status,month);
             return View(expenseForm);
         }
-        
-        public ActionResult ViewReports()
-        {
-            return View();
-        }
 
+
+        public ActionResult CheckBalance()
+        {
+           BudgetViewModel budget = new BudgetViewModel();
+           budget.totalAmountSpent= bud.DepartmentBudget(employee.Department.MonthlyBudget, employee.DepartmentId);
+           budget.totalAmountRemaining = bud.RemainingAmount;
+           return View(budget);
+
+        }
     }
 }
