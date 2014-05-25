8e6e5c3512c942178960d1fdf36be70beb2743ef
JohnQuan <quan.le.john@gmail.com>
2014-05-25 13:22:18 +1000

    Update AccountsController

--- ThreeAmigos_ExpenseManagement/Controllers/AccountsController.cs
+++ ThreeAmigos_ExpenseManagement/Controllers/AccountsController.cs
@@ -6,9 +6,11 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using ThreeAmigos_ExpenseManagement.BusinessLogic;
+using ThreeAmigos_ExpenseManagement.DataAccess;
 using ThreeAmigos_ExpenseManagement.Filters;
 using ThreeAmigos_ExpenseManagement.Models;
 using ThreeAmigos_ExpenseManagement.ViewModels;
+
 
 namespace ThreeAmigos_ExpenseManagement.Controllers
 {
