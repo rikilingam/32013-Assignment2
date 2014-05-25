1974539b2cc47769f4848ec5131dd77db2748620
JohnQuan <quan.le.john@gmail.com>
2014-05-25 12:47:14 +1000

    Update AccountsController

--- ThreeAmigos_ExpenseManagement/Controllers/AccountsController.cs
+++ ThreeAmigos_ExpenseManagement/Controllers/AccountsController.cs
@@ -22,5 +22,86 @@ namespace ThreeAmigos_ExpenseManagement.Controllers
             return View();
         }
 
+
+        IExpenseReportService reportService = new ExpenseReportService();
+        private IEmployeeService employeeService;
+        private Employee employee;
+        BudgetTracker bud;
+        int month = DateTime.Now.Month;
+        int year = DateTime.Now.Year;
+
+        public AccountsController()
+        {
+            reportService = new ExpenseReportService();
+            employeeService = new EmployeeService();
+            bud = new BudgetTracker();
+            employee = employeeService.GetEmployee((int)Membership.GetUser().ProviderUserKey);
+        }
+
+
+        public ActionResult ProcessExpenses(int? itemid, string act)
+        {
+            decimal? reportTotal = 0;
+            ExpenseFormViewModel expenseForm = new ExpenseFormViewModel();
+
+            if (itemid == null)
+            {
+                expenseForm.ExpenseReports = reportService.GetReportsBySupervisor("ApprovedBySupervisor", month);
+                return View(expenseForm);
+            }
+
+            else
+            {
+                if (act == "Approve")
+                {
+                    BudgetViewModel budget = new BudgetViewModel();
+
+                    // compute the total expense of the company in the month
+                    budget.companyAmountSpent = bud.CompanyBudget(month, year);
+                    budget.companyAmountRemaining = bud.RemainingAmount;
+
+                    reportTotal = bud.CheckReportTotal(itemid);
+                    if (!bud.ExceedCompanyBudget(reportTotal))
+                    {
+                        reportService.ProcessReport(itemid, act);
+                    }
+                    expenseForm.ExpenseReports = reportService.GetReportsBySupervisor("ApprovedBySupervisor", month);
+                    return View(expenseForm);
+                }
+                else
+                {
+                    reportService.ProcessReport(itemid, act);
+                    expenseForm.ExpenseReports = reportService.GetReportsBySupervisor("ApprovedBySupervisor", month);
+                    return View(expenseForm);
+                }
+            }
+        }
+
+        public ActionResult ViewReports()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ViewReports(string status)
+        {
+            ExpenseFormViewModel expenseForm = new ExpenseFormViewModel();
+            expenseForm.ExpenseReports = reportService.GetReportsBySupervisor(status, month);
+            return View(expenseForm);
+        }
+
+
+        public ActionResult CheckExpenseApproved()
+        {
+            BudgetViewModel budget = new BudgetViewModel();
+            budget.companyAmountSpent = bud.CompanyBudget(month, year);
+            budget.companyAmountRemaining = bud.RemainingAmount - bud.CompanyBudget(month, year);
+
+            //budget.totalAmountSpent= bud.DepartmentBudget(employee.Department.MonthlyBudget, employee.DepartmentId);
+            //budget.totalAmountRemaining = bud.RemainingAmount;
+            return View(budget);
+        }
+
+
     }
 }
