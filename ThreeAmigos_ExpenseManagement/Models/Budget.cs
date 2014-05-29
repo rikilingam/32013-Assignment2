483f58c89444f9569e75bf8d95dab285a4886fe8
JohnQuan <quan.le.john@gmail.com>
2014-05-29 19:55:56 +1000

    Update Budget.cs

--- ThreeAmigos_ExpenseManagement/Models/Budget.cs
+++ ThreeAmigos_ExpenseManagement/Models/Budget.cs
@@ -11,5 +11,10 @@ namespace ThreeAmigos_ExpenseManagement.Models
         public decimal? totalAmountRemaining { get; set; }
         public decimal? companyAmountSpent { get; set; }
         public decimal? companyAmountRemaining { get; set; }
+        public Budget()
+        {
+            totalAmountRemaining = 2000; // John
+            companyAmountRemaining = 14000;  // John
+        }
     }
 }
