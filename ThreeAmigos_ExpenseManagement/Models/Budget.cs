316bb832070faae76aa380483da457e39607f091
bikrem <bikrem.singh@yahoo.in>
2014-05-29 13:07:42 +1000

    Moved budgetviewmodel to model and renamed to budget

--- ThreeAmigos_ExpenseManagement/Models/Budget.cs
+++ ThreeAmigos_ExpenseManagement/Models/Budget.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ThreeAmigos_ExpenseManagement.ViewModels
+namespace ThreeAmigos_ExpenseManagement.Models
 {
-    public class BudgetViewModel
+    public class Budget
     {
         public decimal? totalAmountSpent { get; set; }
         public decimal? totalAmountRemaining { get; set; }
