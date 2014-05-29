940ac42075b5ec4c0a0db323c42667cd8e1c95be
bikrem <bikrem.singh@yahoo.in>
2014-05-30 00:27:42 +1000

    Added ApproveExpensesViewModel

--- ThreeAmigos_ExpenseManagement/ViewModels/ApproveExpensesViewModel.cs
+++ ThreeAmigos_ExpenseManagement/ViewModels/ApproveExpensesViewModel.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ThreeAmigos_ExpenseManagement.BusinessLogic;
+using ThreeAmigos_ExpenseManagement.Models;
 
 namespace ThreeAmigos_ExpenseManagement.ViewModels
 {
     public class ApproveExpensesViewModel
     {
+        public List<ExpenseReport> ExpenseReports { get; set; }
+        public IBudgetTracker BudgetTracker { get; set; }
+
+        public ApproveExpensesViewModel()
+        {
+            ExpenseReports = new List<ExpenseReport>();
+            BudgetTracker = new BudgetTracker();
+        }
     }
 }
