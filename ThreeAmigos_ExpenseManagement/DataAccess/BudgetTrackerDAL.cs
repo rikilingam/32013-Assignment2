7fc6a602e98c9f76c65e661269767b83ea1a0fb3
JohnQuan <quan.le.john@gmail.com>
2014-05-30 18:29:16 +1000

    Add TotalExpenseProcessByDepartment in BudgetTrackerDAL

--- ThreeAmigos_ExpenseManagement/DataAccess/BudgetTrackerDAL.cs
+++ ThreeAmigos_ExpenseManagement/DataAccess/BudgetTrackerDAL.cs
@@ -54,6 +54,29 @@ namespace ThreeAmigos_ExpenseManagement.DataAccess
         }
 
 
+        public decimal? TotalExpenseProcessByDepartment(int month, int year, Department department)
+        {
+            using (EMEntitiesContext ctx = new EMEntitiesContext())
+            {
+                decimal? total = 0;
+
+                var reports = from expenseReports in ctx.ExpenseReports
+                              where expenseReports.ProcessedDate.Value.Month == month && expenseReports.ProcessedDate.Value.Year == year
+                                    && expenseReports.Status == "ApprovedByAccounts" && department.DepartmentId == expenseReports.Department.DepartmentId
+                              select expenseReports;
+
+                foreach (var expenseItems in reports)
+                {
+                    foreach (var totalamount in expenseItems.ExpenseItems)
+                    {
+                        total = total + totalamount.AudAmount;
+                    }
+                }
+                return total;
+            }
+        }
+
+
 
         public decimal? CheckReportTotal(int? expenseId)
         {
