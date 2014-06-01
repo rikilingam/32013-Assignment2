822b88bc2d98ccb05020a7c8fab33e0c25a01232
bikrem <bikrem.singh@yahoo.in>
2014-06-02 03:00:25 +1000

    Added MockBudgetService

---
+++ ThreeAmigos_ExpenseManagement.Tests/MockBusinessLogic/MockBudgetService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThreeAmigos_ExpenseManagement.BusinessLogic;
+using ThreeAmigos_ExpenseManagement.Models;
+
+
+namespace ThreeAmigos_ExpenseManagement.Tests.MockBusinessLogic
+{
+    class MockBudgetService:IBudgetService
+    {
+        Department department; 
+        public MockBudgetService(Department department)
+        {
+             this.department = department;
+             Budget = new Budget(department.MonthlyBudget);
+            }
+
+        public Budget Budget
+        {
+            get
+             {
+              return Budget =new Budget(department.MonthlyBudget);    
+             }
+            set
+            {
+                Budget budget = new Budget(department.MonthlyBudget);
+                budget.Spent = 4000;
+            }
+        }
+
+        public void SetBudgetSpent(int month, int year)
+        {
+        }
+
+        public bool IsBudgetExceeded(decimal? amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal? GetDepartmentBudgetRemain(int month, int year, Department department)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
