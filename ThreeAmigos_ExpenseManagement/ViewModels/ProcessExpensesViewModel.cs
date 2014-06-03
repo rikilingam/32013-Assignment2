56213e1040863b1252e49ab173b1bb02a0f5b88a
bikrem <bikrem.singh@yahoo.in>
2014-06-03 14:02:53 +1000

    Removed comments

--- ThreeAmigos_ExpenseManagement/ViewModels/ProcessExpensesViewModel.cs
+++ ThreeAmigos_ExpenseManagement/ViewModels/ProcessExpensesViewModel.cs
@@ -11,11 +11,5 @@ namespace ThreeAmigos_ExpenseManagement.ViewModels
     {
         public List<ExpenseReport> ExpenseReports { get; set; }
         public IBudgetService BudgetTracker { get; set; }
-
-        //public ProcessExpensesViewModel()
-        //{
-        //    ExpenseReports = new List<ExpenseReport>();
-        //    BudgetTracker = new BudgetTracker();
-        //}
     }
 }
